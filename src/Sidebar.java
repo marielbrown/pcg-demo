116a535e712bc4ce4508595d2dcdf867fb3d2811
Marie Brown <mariebrown.mlb@gmail.com>
2024-05-20 17:01:22 +0100

    bugifx: button handler setup corretly

--- src/Sidebar.java
+++ src/Sidebar.java
@@ -3,9 +3,9 @@ import processing.core.PApplet;
 public class Sidebar extends UIElement{
 
     public Button[] buttons;
-    EventListener buttonHandler = new ToolButtonHandler();
+    EventListener buttonHandler;
 
-    Sidebar(PApplet app) {
+    Sidebar(ContentGenerationDemoTool app) {
         width = app.width * 0.2f;   // 1/5 size of screen
         height = app.height;
         position.x = 0;
@@ -14,6 +14,8 @@ public class Sidebar extends UIElement{
 
         buttons = new Button[1];
         buttons[0] = new Button(0, position.x + 40, position.y + 45, 200, 40, "PCG Method 1");
+
+        buttonHandler = new ToolButtonHandler(app);
 
         for (Button button: buttons){
             button.addEventListener(buttonHandler);
