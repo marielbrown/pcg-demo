063f19aef5b0a08ddf5572fcec4d0fc180b58ceb
Marie Brown <mariebrown.mlb@gmail.com>
2024-05-23 17:08:10 +0100

    Canvas and map placement separated to allow for resizing

--- src/Canvas.java
+++ src/Canvas.java
@@ -8,11 +8,21 @@ public class Canvas extends UIElement{
     Button[] buttons;
     EventListener buttonHandler;
 
+    PVector mapPosition = new PVector();
+    float mapWidth, mapHeight;
+
+
+
     Canvas(ContentGenerationDemoTool app){
-        width = app.width * 0.6f;   // 3/5 size of screen
-        height = app.height * 0.6f;
-        position.x = (app.width * 1.2f - width) / 2;
-        position.y = (app.height - height) / 2;
+        position.x = app.width * 0.2f - 15;
+        position.y = 0;
+        width = app.width - (position.x * 2);
+        height = app.height;
+
+        mapWidth = width * 0.8f;   // centred in canvas area
+        mapHeight = height * 0.6f;
+        mapPosition.x = position.x + (width - mapWidth) / 2;
+        mapPosition.y = (height - mapHeight) / 2;
 
         buttonHandler = new CanvasButtonHandler(app);
 
@@ -21,7 +31,7 @@ public class Canvas extends UIElement{
         buttons = new Button[buttonText.length]; // todo: temporary placement - move to toolbar class?
         int buttonWidth = 100;
         for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new Button(i, position.x + buttonWidth * i, position.y + height + 10, buttonWidth, 50, buttonText[i]);
+            buttons[i] = new Button(i, mapPosition.x + buttonWidth * i, mapPosition.y + mapHeight + 10, buttonWidth, 50, buttonText[i]);
             buttons[i].addEventListener(buttonHandler);
         }
 
@@ -32,8 +42,7 @@ public class Canvas extends UIElement{
     @Override
     public void render(PApplet app) {
         app.stroke(0);
-        app.fill(120, 120, 120);
-        app.rect(position.x, position.y, width, height);
+
 
         for (Button button: buttons){
             button.render(app);
@@ -44,8 +53,8 @@ public class Canvas extends UIElement{
             return;
         }
 
-        float cellHeight = height / map[0].length;
-        float cellWidth = width / map.length;
+        float cellHeight = mapHeight / map[0].length;
+        float cellWidth = mapWidth / map.length;
 
         for (int x = 0; x < map.length; x++) {
             for (int y = 0; y < map[0].length; y++) {
@@ -54,9 +63,12 @@ public class Canvas extends UIElement{
                 } else {
                     app.fill(200, 10, 100);
                 }
-                app.rect(x * cellWidth + position.x, y * cellHeight + position.y, cellWidth, cellHeight);
+                app.rect(x * cellWidth + mapPosition.x, y * cellHeight + mapPosition.y, cellWidth, cellHeight);
             }
         }
+
+        //app.fill(50, 80, 120);    // for testing
+        //app.rect(position.x, position.y, width, height);
     }
 
     public void setMap(boolean[][] map) {
