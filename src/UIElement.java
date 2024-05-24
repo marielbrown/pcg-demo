f126591e1a2606a107fd2a610fe07f344bab057b
Marie Brown <mariebrown.mlb@gmail.com>
2024-05-24 17:32:03 +0100

    default colour added to UI element

--- src/UIElement.java
+++ src/UIElement.java
@@ -1,9 +1,11 @@
 import processing.core.PApplet;
 import processing.core.PVector;
 
+
 public abstract class UIElement {
     protected PVector position = new PVector();
     protected float width, height;
+    protected int defaultColour = -8882056; // obtained from processing color function
 
     boolean isMouseOver(int mouseX, int mouseY){
         return mouseX > position.x && mouseX < position.x + width &&
